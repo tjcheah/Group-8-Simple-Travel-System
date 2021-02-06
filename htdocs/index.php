a1452eefe8594d4c59319f2164a329ce7a9334fa
kopidadoudou <kelvintee1999@gmail.com>
2021-02-07 03:07:37 +0800

    query to save image

--- htdocs/index.php
+++ htdocs/index.php
@@ -134,6 +134,32 @@
 				</div>
 				<div class="my-5 col-8">
 					<?php $row = mysqli_fetch_array($user); ?>
+					<?php
+						include("config.php");
+						if(!empty('file') && isset($_POST['but_upload'])){
+							$name = $_FILES['file']['name'];
+							$target_dir = "upload/";
+							$target_file = $target_dir . basename($_FILES["file"]["name"]);
+
+							// Select file type
+							$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+							// Valid file extensions
+							$extensions_arr = array("jpg","jpeg","png","gif");
+
+							// Check extension
+							if( in_array($imageFileType,$extensions_arr) ){
+							
+								// Convert to base64 
+								$image_base64 = base64_encode(file_get_contents($_FILES['file']['tmp_name']) );
+								$image = 'data:image/'.$imageFileType.';base64,'.$image_base64;
+								// Insert record
+								$id = $row['user_id'];
+								$query = "update user_detail set images = '".$image."' where user_id = '".$id."' ";
+								mysqli_query($con,$query);
+							}
+						}
+					?>
 					<form method="POST" action="/php/update_user.php">
 					  <div class="form-group">
 						<label>Image: </label>
